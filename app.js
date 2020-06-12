e6ab2103805ca83799d730d3ee9a7e2d22ae66b3
anand-saurabh <saurabh.anand@ufl.edu>
2020-06-12 01:48:35 -0400

    added feature to mark all done, delete, add todo items

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 const app = new Vue({
 el: '#app',
 data: {
-  title: 'To Do Items'
+  title: 'To Do Items',
+  newTodo: '',
+  todos: []
+},
+methods:
+{
+  addTodo()
+  {
+    this.todos.push({
+      title: this.newTodo,
+      done : false
+    })
+    this.newTodo = '';
+  },
+  removeTodo(todo)
+  {
+    const index = this.todos.indexOf(todo);
+    this.todos.splice(index, 1);
+  },
+  allDone()
+  {
+    this.todos.forEach(todo => {
+      todo.done = true;
+    });
+  }
 }
 });
